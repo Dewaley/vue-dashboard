856836f4f752db1d6b57080e2f08088c6fecb062
Dewaley <adewaleademilua@gmail.com>
2023-09-04 13:02:52 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/Navbar.vue";
+import { ref } from "vue";
+import { useFullscreen } from "@vueuse/core";
+
+const el = (ref < HTMLElement) | (null > null);
+const { toggle } = useFullscreen();
+</script>
+
+<template>
+  <div ref="el">
+    <Navbar :toggle="toggle" />
+    <!-- <RouterView /> -->
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<script setup>
+import {
+  Bars3Icon,
+  MagnifyingGlassIcon,
+  SquaresPlusIcon,
+  ArrowsPointingOutIcon,
+  BellIcon,
+  Cog6ToothIcon,
+} from "@heroicons/vue/24/outline";
+import { defineProps } from "vue";
+
+const props = defineProps(["toggle"]);
+</script>
+
+<template>
+  <nav>
+    <div class="left-side">
+      <Bars3Icon class="icon" />
+      <form @submit.prevent="">
+        <MagnifyingGlassIcon class="icon" />
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Search..."
+          class="search"
+        />
+      </form>
+      a
+    </div>
+    <div class="right-side">
+      <SquaresPlusIcon class="icon" />
+      <ArrowsPointingOutIcon class="icon" @click="toggle" />
+      <span class="bell"><BellIcon class="icon" /></span>
+      <span class="acc-details"></span>
+      <Cog6ToothIcon class="icon" />
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.icon {
+  color: var(--dark-text);
+  height: 3rem;
+}
+</style>
